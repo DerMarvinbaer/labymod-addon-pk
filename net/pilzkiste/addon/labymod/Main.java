181502995aaa32a3404203cca1d84ddbdf4984dc
DerEisbaer <nanana.7@web.de>
2018-02-07 17:37:04 +0100

    initial commit

---
+++ net/pilzkiste/addon/labymod/Main.java
@@ -0,0 +1,53 @@
+package net.pilzkiste.addon.labymod;
+
+import net.labymod.api.LabyModAddon;
+import net.labymod.ingamegui.ModuleCategory;
+import net.labymod.ingamegui.ModuleCategoryRegistry;
+import net.labymod.settings.elements.BooleanElement;
+import net.labymod.settings.elements.ControlElement;
+import net.labymod.settings.elements.SettingsElement;
+import net.labymod.utils.Material;
+import net.pilzkiste.addon.labymod.module.PilzkisteModules;
+
+import java.util.List;
+
+public class Main extends LabyModAddon {
+
+    /*
+        This Addon is for the Minecraft-Server Pilzkiste.net
+        All rights reserved
+     */
+
+    public static boolean knockbattle;
+    public static boolean knockffa;
+    public static boolean rebuild;
+    public static boolean nickname;
+
+    @Override
+    public void onEnable() {
+        System.out.print("Enabled Pilzkiste Addon");
+        this.getApi().registerServerSupport(this, new PilzkisteModules());
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+    @Override
+    public void loadConfig() {
+        knockbattle = getConfig().has( "enabledKnockBattle" ) ? getConfig().get( "enabledKnockBattle" ).getAsBoolean() : true;
+        knockffa = getConfig().has( "enabledKnockFFA" ) ? getConfig().get( "enabledKnockFFA" ).getAsBoolean() : true;
+        rebuild = getConfig().has( "enabledRebuild" ) ? getConfig().get( "enabledRebuild" ).getAsBoolean() : true;
+    }
+
+
+    @Override
+    protected void fillSettings(final List<SettingsElement> list) {
+        list.add( new BooleanElement("Enabled KnockBattle Feature", this, new ControlElement.IconData(Material.DIAMOND_HELMET), "enabledKnockBattle", this.knockbattle) );
+        list.add( new BooleanElement("Enabled KnockFFA Feature", this, new ControlElement.IconData(Material.STICK), "enabledKnockFFA", this.knockffa) );
+        list.add( new BooleanElement("Enabled Rebuild Feature", this, new ControlElement.IconData(Material.GRASS), "enabledRebuild", this.rebuild) );
+        list.add( new BooleanElement( "Enabled Nickname", this, new ControlElement.IconData(Material.NAME_TAG), "enabledNickname", this.nickname));
+    }
+
+}
---
+++ net/pilzkiste/addon/labymod/module/PilzkisteModules.java
@@ -0,0 +1,109 @@
+package net.pilzkiste.addon.labymod.module;
+
+import com.google.gson.JsonObject;
+import net.labymod.api.events.TabListEvent;
+import net.labymod.ingamegui.moduletypes.ColoredTextModule;
+import net.labymod.servermanager.ChatDisplayAction;
+import net.labymod.servermanager.Server;
+import net.labymod.settings.elements.BooleanElement;
+import net.labymod.settings.elements.ControlElement;
+import net.labymod.settings.elements.SettingsElement;
+import net.labymod.utils.Material;
+import net.minecraft.client.multiplayer.ServerData;
+import net.minecraft.network.PacketBuffer;
+import net.pilzkiste.addon.labymod.Main;
+import org.lwjgl.Sys;
+
+import java.util.Collections;
+import java.util.List;
+
+public class PilzkisteModules extends Server {
+
+    public static String nickname = "Error";
+    private String points = "-9999999";
+    private String killstreak = "-5";
+    private String level = "-5";
+
+    public PilzkisteModules() {
+        super("pilzkiste", "pilzkiste.net", "franzcraft.net", "pilzkiste.de", "pilzkiste.eu");
+    }
+
+    @Override
+    public void onJoin(final ServerData serverData) {
+
+    }
+
+    @Override
+    public ChatDisplayAction handleChatMessage(final String s, final String s1) throws Exception {
+        return null;
+    }
+
+
+    @Override
+    public void handlePluginMessage(final String channelName, final PacketBuffer packetBuffer) throws Exception {
+        if(channelName.equals("nickName")) {
+            nickname = new String(packetBuffer.array());
+        }
+
+        if(channelName.equals("kBattle_points")) {
+            points = new String(packetBuffer.array());
+        }
+
+        if(channelName.equals("kFFA_killstreak")) {
+            killstreak = new String(packetBuffer.array());
+        }
+
+        if(channelName.equals("reBuild_level")) {
+            level = new String(packetBuffer.array());
+        }
+    }
+
+    @Override
+    public void handleTabInfoMessage(final TabListEvent.Type type, final String s, final String s1) throws Exception {
+
+    }
+
+    @Override
+    protected void initConfig(JsonObject config) {
+    }
+
+    @Override
+    public void fillSubSettings(final List<SettingsElement> list) {
+
+    }
+
+    @Override
+    public void addModuleLines( List<DisplayLine> lines ) {
+
+        if(Main.nickname) {
+            if(!nickname.equals("Error")) {
+                DisplayLine dl = new DisplayLine( "Nick", Collections.singletonList(ColoredTextModule.Text.getText(nickname)));
+                lines.add(dl);
+            }
+        }
+
+        if(Main.knockbattle) {
+            if(!points.equals("-9999999")) {
+                DisplayLine dl = new DisplayLine( "Punkte", Collections.singletonList(ColoredTextModule.Text.getText(String.valueOf(points))));
+                lines.add(dl);              
+            }
+        }
+
+        if(Main.knockffa) {
+            if(!killstreak.equals("-5")) {
+                DisplayLine dl = new DisplayLine( "Killstreak", Collections.singletonList(ColoredTextModule.Text.getText(String.valueOf(killstreak))));
+                lines.add(dl);
+            }
+        }
+
+        if(Main.rebuild) {
+            if(!level.equals("-5")) {
+                DisplayLine dl = new DisplayLine( "Level", Collections.singletonList(ColoredTextModule.Text.getText(String.valueOf(level))));
+                lines.add(dl);
+            }
+        }
+
+        super.addModuleLines(lines);
+    }
+
+}
